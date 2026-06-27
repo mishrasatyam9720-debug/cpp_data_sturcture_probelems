c9c1daef09660ac673f4b80f551c83b95a721859
mishra.9720 <mishra.satyam.9720@gmail.com>
2026-06-27 13:17:13 +0530

    inversion count added

--- array.cpp
+++ array.cpp
@@ -325,4 +325,65 @@ using namespace std ;
 
 // }
 
-// next permutation 
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int merge(vector<int>& arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    int cnt = 0;
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            cnt += (mid - left + 1);
+            right++;
+        }
+    }
+
+    while (left <= mid) {
+        temp.push_back(arr[left++]);
+    }
+
+    while (right <= high) {
+        temp.push_back(arr[right++]);
+    }
+
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+
+    return cnt;
+}
+
+int mergeSort(vector<int>& arr, int low, int high) {
+    if (low >= high)
+        return 0;
+
+    int mid = low + (high - low) / 2;
+
+    int cnt = 0;
+    cnt += mergeSort(arr, low, mid);
+    cnt += mergeSort(arr, mid + 1, high);
+    cnt += merge(arr, low, mid, high);
+
+    return cnt;
+}
+
+int inversionCount(vector<int>& arr) {
+    return mergeSort(arr, 0, arr.size() - 1);
+}
+
+int main() {
+    vector<int> arr = {5, 3, 2, 4, 1};
+
+    cout << "Inversion Count = " << inversionCount(arr) << endl;
+
+    return 0;
+}
+
